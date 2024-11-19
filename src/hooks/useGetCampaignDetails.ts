103e5d8c0166bfcebad24f6f44c83a28fb4cefca
khayss <forendals@gmail.com>
2024-11-19 10:44:19 +0100

    feat: add get campaign details

---
+++ src/hooks/useGetCampaignDetails.ts
@@ -0,0 +1,16 @@
+import { abi } from "../context/abi";
+import { contractAddress } from "../context/contractAddress";
+import { useReadContract } from "wagmi";
+
+export const useGetCampaignDetails = () => {
+  const getCampaignDetails = async (campaignId: string) => {
+    return useReadContract({
+      abi,
+      address: contractAddress,
+      functionName: "getCampaignDetails",
+      args: [campaignId],
+    });
+  };
+
+  return { getCampaignDetails };
+};
